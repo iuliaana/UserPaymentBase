5fa9700a6620634ad7cf24e686072839424436d4
Yulia <82370585+iuliaana@users.noreply.github.com>
2022-05-20 11:45:35 +0300

    Add files via upload

---
+++ GribovaPUB/GribovaPUB/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GribovaPUB
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    class AppData
+    {
+        public static UserPaymentBaseEntities Context = new UserPaymentBaseEntities();
+        public static 
+    }
+}
---
+++ GribovaPUB/GribovaPUB/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GribovaPUB
+{
+    /// <summary>
+    /// Actions for closing
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboLogin.ItemSource = AppData.Context.User.ToList();
+        }
+
+        private void BtnLogin_Click (object sender, RoutedEventArgs e)
+        {
+            if ComboLogin.SelectedItem i User currentUser)
+                {
+                if (PBoxPassword.Password == currentUser.Password)
+                {
+                    AppData.CurrentUser = CurrentUser;
+                    var paymentWindow = new PaymentsWindow();
+                    paymentWindow.Owner = this;
+                    paymentWindow.Show();
+                    Hide();
+                }
+                else 
+                    MessageBox.Show("Неверный пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
